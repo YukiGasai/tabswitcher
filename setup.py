810f3f31709329f5df2275d39bee6e3bb779135b
YukiGasai <r.lindede@googlemail.com>
2024-05-26 00:31:38 +0200

    Working on Installer

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tabswitcher',
-    version='0.0.8',
+    version='0.0.9',
     packages=['tabswitcher'],
     package_dir={'tabswitcher': 'src/tabswitcher'},
     package_data={'tabswitcher': ['assets/*']},
--- src/tabswitcher/assets/install.bat
+++ src/tabswitcher/assets/install.bat
@@ -27,18 +27,44 @@ if '%errorlevel%' NEQ '0' (
 :: Insatll the mediator in the browser
 call bt install
 
+:: Get the User ID
+for /f "tokens=1* delims= " %%i in ('whoami /user /fo list ^| findstr /B /C:"SID:"') do set "UserID=%%j"
+
+echo "User ID: %UserID%"
+
+for /f "tokens=* USEBACKQ" %%F in (`powershell -Command "(Get-Command bt_mediator).Source"`) do (
+   SET commandPath=%%F
+)
+
 :: Create a scheduled task to run the BroTab Service
 echo Creating scheduled task for BroTab Service
 set "XML_PATH=%SCRIPT_DIR%/brotab_service.xml"
+
+:: Update the Command and User ID in the XML file
+powershell -Command "$xml = [xml](Get-Content '%XML_PATH%'); $xml.Task.Actions.Exec.Command = '%commandPath%'; $xml.Task.Principals.Principal.UserId = '%UserID%'; $xml.Save('%XML_PATH%')"
+
+:: Create the scheduled task
 schtasks /Create /XML "%XML_PATH%" /TN "BroTab Service"
 
 :: Start the BroTab Service task
 echo Starting BroTab Service
 schtasks /Run /TN "BroTab Service"
 
+
+
+
+for /f "tokens=* USEBACKQ" %%F in (`powershell -Command "(Get-Command tabswitcher).Source"`) do (
+   SET commandPath=%%F
+)
+
 :: Create a scheduled task to run the Tabswitcher Logger
-echo Creating scheduled task for BroTab Service
+echo Creating scheduled task for Tabswitcher Logger
 set "XML_PATH=%SCRIPT_DIR%/tabswitcher_service.xml"
+
+:: Update the Command and User ID in the XML file
+powershell -Command "$xml = [xml](Get-Content '%XML_PATH%'); $xml.Task.Actions.Exec.Command = '%commandPath%'; $xml.Task.Principals.Principal.UserId = '%UserID%'; $xml.Save('%XML_PATH%')"
+
+:: Create the scheduled task
 schtasks /Create /XML "%XML_PATH%" /TN "Tabswitcher Logger"
 
 :: Start the Tabswitcher Logger task
