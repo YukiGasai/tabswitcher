bb0d6f7c021134e149ba6ef70855e5f1dedaf555
YukiGasai <r.lindede@googlemail.com>
2024-05-25 23:09:51 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tabswitcher',
-    version='0.0.6',
+    version='0.0.7',
     packages=['tabswitcher'],
     package_dir={'tabswitcher': 'src/tabswitcher'},
     package_data={'tabswitcher': ['assets/*']},
