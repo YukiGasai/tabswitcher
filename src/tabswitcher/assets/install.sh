b5f210682f9b8d653e63bc9795b371c5a74896bc
YukiGasai <r.lindede@googlemail.com>
2024-05-28 00:49:22 +0200

    Update ubuntu install

--- src/tabswitcher/assets/install.sh
+++ src/tabswitcher/assets/install.sh
@@ -1,21 +1,9 @@
 #!/bin/sh
 
-# Get the username of the user who invoked sudo
-if [ $SUDO_USER ]; then USER=$SUDO_USER; else USER=`whoami`; fi
+# Install the brotab meditator
+bt install
 
-# Check if script is run with sudo
-if [ "$(id -u)" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
-# Check if tabswitcher is installed
-if ! sudo -u $USER command -v tabswitcher >/dev/null
-then
-    echo "tabswitcher could not be found. Please install it first."
-    exit
-fi
-
+# Install a startup tast for the active tab logger
 (crontab -l 2>/dev/null; echo "@reboot tabswitcher --startlogger") | crontab -
 
 echo "Installed cron job to start tabswitcher on system start."
